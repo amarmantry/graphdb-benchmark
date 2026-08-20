ca9ee1365aba2bf9f90dbf3bef279782cc63532b
amarmantry <mantryamar@gmail.com>
2026-08-20 07:18:23 +0530

    chore: initial Spring Boot project scaffold with Neo4j and Jedis drivers

---
+++ src/main/java/com/amarmantry/graphdb_benchmark/GraphdbBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package com.amarmantry.graphdb_benchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphdbBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphdbBenchmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amarmantry/graphdb_benchmark/GraphdbBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarmantry.graphdb_benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphdbBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
